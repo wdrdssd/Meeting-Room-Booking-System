8ca62458633f2a668e5fda633ce6a30b46f4d10a
wdrdssd <2786888512@qq.com>
2026-03-18 16:43:31 +0800

    login-02

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,8 @@ const router = new VueRouter({
     mode:'history',
     routes:[
         { name:'login',path:'/login',component:()=>import('../views/user/Login.vue')},
+        { name:'userHomepage',path:'/user/homepage',component:()=>import('../views/user/Homepage.vue')},
+        { name:'adminHomepage',path:'/admin/homepage',component:()=>import('../views/admin/Homepage.vue')},
     ],
     linkActiveClass:'on'
 })
---
+++ frontend/src/views/user/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>用户的主页</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'userHomepage'
+    }
+</script>
+
+<style >
+
+</style>
